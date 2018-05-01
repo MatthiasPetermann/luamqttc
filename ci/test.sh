4a251d3979405fc1d35359366fea0f5a22eeccce
Matthias Petermann <matthias@petermann-it.de>
2018-05-01 06:35:11 +0200

    Removed codeship and docker, added luadist standard cmake cfg

--- ci/test.sh
+++
@@ -1,7 +0,0 @@
-luarocks make
-
-git clone https://github.com/eclipse/paho.mqtt.testing.git
-oldpwd=`pwd`
-cd paho.mqtt.testing/interoperability && python3 startbroker.py &
-sleep 10
-cd $oldpwd && lua tests/client_test.lua
